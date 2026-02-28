8fa4c5ca6b93f5977c522826f1eb35d039ed30be
neptunhiker <bastiusa47@yahoo.de>
2026-02-28 20:48:31 +0100

    Add a landing page

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
+    path('', TemplateView.as_view(template_name='landing.html'), name='landing'),
 ]
